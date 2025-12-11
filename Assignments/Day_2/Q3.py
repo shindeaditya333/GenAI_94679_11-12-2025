40138743bbd90cb71dec30dbabc37b533effff51
Aditya Shinde <shindeaditya9354@gmail.com>
2025-12-11 22:27:48 +0530

    Final Changes

--- Assignments/Day_2/Q3.py
+++ Assignments/Day_2/Q3.py
@@ -13,7 +13,12 @@ import pandas as pd
 df=pd.read_csv("D:\Sunbeam_Internship_GenAI\GIT_Sunbeam\GenAI_94679_11-12-2025\GenAI_94679_11-12-2025\Assignments\Day_2\CSVs\products.csv")
 
 print(df)
+print("\nEach row in clean format:")
+for index, row in df.iterrows():
+    print(f"ID: {row['product_id']}, Name: {row['product_name']}, Price: {row['price']}, Category: {row['category']}, Qty: {row['quantity']}")
+
 print("Total number of rows : ",len(df))
 print("Total number of products priced above 500 : \n",df[df['price']>500])
 print("Average price of all products : \n",df['price'].mean())
 print("List all products belonging to a specific category (user input) : \n",df[df['category'] == 'Electronics'])
+print("Total quantity of all items in stock : ", df['quantity'].sum())
