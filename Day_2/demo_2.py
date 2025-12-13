0682aa46a51c0a203740aac491faf554ce71d97b
Aditya Shinde <shindeaditya9354@gmail.com>
2025-12-13 08:44:01 +0530

    Final

--- Day_2/demo_2.py
+++ Day_2/demo_2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pandasql as ps
 
-path="D:\Sunbeam_Internship_GenAI\GIT_Sunbeam\GenAI_94679_11-12-2025\GenAI_94679_11-12-2025\Day_2\emp_hdr.csv"
+path="emp_hdr.csv"
 df=pd.read_csv(path)
 print(df)
 
